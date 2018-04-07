2addbc7b91c3bfcd709d6fb0cef6621fd4e2088d
jomanahosam <jomana.hosam@yahoo.com>
2018-04-07 14:19:45 +0200

    first commit
    added project

---
+++ avatar/Assets/scripts/EnemyMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+	Transform Player;
+	UnityEngine.AI.NavMeshAgent nav;
+     
+	// Use this for initialization
+	void Awake () 
+	{
+		Player = GameObject.FindWithTag ("Player").transform; 
+		nav = GetComponent<UnityEngine.AI.NavMeshAgent> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		nav.SetDestination (Player.position);
+		
+	}
+}
